d5eb860fa45dd9deb002c541640d60a33ddbdccf
Thiago <rp.thiagoluiz@gmail.com>
2020-10-13 19:34:23 -0300

    pause.N

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
+import multer from 'multer'
+
+import uploadConfig from './config/upload'
 import OrphanagesController from './controllers/OrphanagesController'
 
 
-const routes = Router();
+const routes = Router()
+const upload = multer(uploadConfig)
 
 /*
    <-! Arquitetura - MVC
@@ -18,6 +22,6 @@ const routes = Router();
 
 routes.get('/orphanages', OrphanagesController.index)
 routes.get('/orphanages/:id',OrphanagesController.show)
-routes.post('/orphanages', OrphanagesController.create)
+routes.post('/orphanages', upload.array('images'),OrphanagesController.create)
 
 export default routes
